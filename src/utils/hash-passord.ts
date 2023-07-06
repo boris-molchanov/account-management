efdfc2b4fd026feb37cd558803682356033f6bf3
boris.molchanov <boris.molchanov@itp-it.com>
2023-07-06 18:24:27 +0400

    remove jwt

--- src/utils/hash-passord.ts
+++
@@ -1,6 +0,0 @@
-import * as bcrypt from "bcryptjs";
-
-export const hashPassword = async (password: string): Promise<string> => {
-    const randomSalt = await bcrypt.genSalt(12)
-    return bcrypt.hash(password, randomSalt);
-};
--- src/utils/hash-token.ts
+++
@@ -1,5 +0,0 @@
-import crypto from "crypto";
-
-export const hashToken = (token: any) => {
-  return crypto.createHash('sha512').update(token).digest('hex');
-}
